eda26d54833a4304859cf22cc737a17ce85157bc
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-08-11 18:47:03 +0200

    first commit f01

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,22 @@
+const LoginForm = () => {
+  return (
+    <div>
+      <h1>
+        Bienvenue chez nous <a href="/login">Connectez vous</a>
+      </h1>
+      <form action="">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="entrez votre prénom ..."
+          required
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Accéder à votre espace
+        </button>
+      </form>
+    </div>
+  )
+}
+export default LoginForm
